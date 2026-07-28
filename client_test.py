ce8d0936683e0d0cf779cc7d80515f355b696502
Fuck Off <fuckOff@gmail.com>
2026-07-28 20:02:40 +0200

    smoothened

--- client_test.py
+++ client_test.py
@@ -2,6 +2,7 @@ import socket
 import json
 import time
 import p2p
+import random
 
 
 MY_ADDR = ("0.0.0.0", 0)
@@ -33,7 +34,30 @@ print(f"other peer is: {target_addr[0]}:{target_addr[1]}")
 
 connection = p2p.P2PConnection(sock, target_addr[0], target_addr[1])
 
+nums = {}
+send_back = []
 while 1:
-    connection.send(f"{time.time()}".encode())
-    data = connection.recv()
-    print(f"diff {time.time() - float(data.decode())}")
+    try:
+
+        num = random.randrange(1, 100000)
+        nums[num] = time.time()
+
+        data = json.dumps({
+            "echo": send_back,
+            "send": num
+        }).encode("utf-8")
+
+        connection.send(data)
+        data = connection.recv()
+
+        data = json.loads(data)
+        send_back = []
+        send_back.append(data["send"])
+
+        for echoed_num in data["echo"]:
+            print( f"{round((time.time() - nums[echoed_num])*1000, 3)} ms -------> {echoed_num}" )
+            del nums[echoed_num]
+
+
+    except Exception as e:
+        print(f"[ERROR] {e}")
